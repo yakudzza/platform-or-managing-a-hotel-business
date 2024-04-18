29cb918ed348bb30d02de619a9b45e739168e0b1
yakudzza <andrewlutov1@gmail.com>
2024-04-18 13:52:43 +0300

    Initial commit

---
+++ src/main/java/com/example/platformormanagingahotel/business/PlatformOrManagingAHotelBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.example.platformormanagingahotel.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformOrManagingAHotelBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformOrManagingAHotelBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/platformormanagingahotel/business/PlatformOrManagingAHotelBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platformormanagingahotel.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformOrManagingAHotelBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
