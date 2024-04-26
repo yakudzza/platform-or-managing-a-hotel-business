b3bc91602d219320217e05a90076ce2c87f97b01
yakudzza <andrewlutov1@gmail.com>
2024-04-26 03:09:19 +0300

    О

---
+++ src/main/java/com/example/platformormanagingahotel/business/api/dto/UserDto.java
@@ -0,0 +1,36 @@
+package com.example.platformormanagingahotel.business.api.dto;
+
+
+import com.example.platformormanagingahotel.business.api.entities.Role;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.class)
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private String phoneNumber;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private LocalDate dateOfBirth;
+    private String passportSeries;
+    private String passportNumber;
+    private String citizenship;
+    private boolean isActive;
+    private Set<Role> roles;
+}
