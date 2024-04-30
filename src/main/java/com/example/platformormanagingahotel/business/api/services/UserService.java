24bdbc87df38a9a5b7a5e2b41879620c9a19504d
yakudzza <andrewlutov1@gmail.com>
2024-04-30 22:58:45 +0300

    Merge changes from main into ваша_ветка

--- src/main/java/com/example/platformormanagingahotel/business/api/services/UserService.java
+++ src/main/java/com/example/platformormanagingahotel/business/api/services/UserService.java
@@ -1,66 +1,15 @@
 package com.example.platformormanagingahotel.business.api.services;
 
-import com.example.platformormanagingahotel.business.api.entities.Role;
-import com.example.platformormanagingahotel.business.api.entities.UserEntity;
-import com.example.platformormanagingahotel.business.api.exceptions.NotFoundException;
-import com.example.platformormanagingahotel.business.api.repositories.RoleRepository;
-import com.example.platformormanagingahotel.business.api.repositories.UserRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
 
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    UserRepository userRepository;
-
-    RoleRepository roleRepository;
-
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByEmail(email);
-
-        if(user == null){
-            throw new NotFoundException("User not found");
-        }
-        return user;
-
-    }
-
-    public UserEntity findUserById(Long userId) {
-        Optional<UserEntity> userFromDb = userRepository.findById(userId);
-        return userFromDb.orElse(new UserEntity());
-    }
-
-    public boolean saveUser(UserEntity user){
-        UserEntity userEntity = userRepository.findByEmail(user.getEmail());
-
-        if (userEntity != null){
-            return false;
-        }
-
-        userEntity.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
-        userEntity.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        userRepository.saveAndFlush(user);
-    }
-
+public class UserService {
 
 }
