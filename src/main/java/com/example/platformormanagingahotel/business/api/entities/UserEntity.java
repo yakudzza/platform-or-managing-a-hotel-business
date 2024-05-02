f03812bdf4f6e4bbfdc028c8544eaae640e1d024
yakudzza <andrewlutov1@gmail.com>
2024-05-03 00:13:36 +0300

    qa

--- src/main/java/com/example/platformormanagingahotel/business/api/entities/UserEntity.java
+++ src/main/java/com/example/platformormanagingahotel/business/api/entities/UserEntity.java
@@ -1,17 +1,12 @@
 package com.example.platformormanagingahotel.business.api.entities;
 
 
-import com.example.platformormanagingahotel.business.api.entities.enums.Roles;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -21,7 +16,7 @@ import java.util.Set;
 @Data
 @Entity
 @Table(name = "users")
-public class UserEntity implements UserDetails {
+public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -47,49 +42,22 @@ public class UserEntity implements UserDetails {
     @Column(unique = true, updatable = false)
     private String passportNumber;
 
-    //гражданство
     private String citizenship;
 
     @Builder.Default
     private Instant createdAt = Instant.now();
-    @Enumerated(EnumType.STRING)
-    private Set<Roles> roles = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Collection<RoleEntity> roles;
+
+
     private boolean active;
 
 
 
-    //Security
-    public boolean isAdmin() {
-        return roles.contains(Roles.ROLE_ADMIN);
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return active;
-    }
 }
