ff930828da53a3d3f6c686fe3e78e182096eb38d
yakudzza <andrewlutov1@gmail.com>
2024-05-03 15:58:31 +0300

    z

--- src/main/java/com/example/platformormanagingahotel/business/api/repositories/UserRepository.java
+++ src/main/java/com/example/platformormanagingahotel/business/api/repositories/UserRepository.java
@@ -11,6 +11,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
    Optional<UserEntity> findByEmail(String email);
-
-   boolean existsByEmail(String email);
 }
